905db06dc92f725fcfeddbc5e96b56e25e9009da
PRASANNA KUMAR M <prasannakumarmofficial@gmail.com>
2024-05-07 13:19:13 +0530

    Update armstrong.cpp

--- 01.05.2024/Basic/armstrong.cpp
+++ 01.05.2024/Basic/armstrong.cpp
@@ -7,13 +7,13 @@ int main() {
     scanf("%d", &a);
     num = a; // Store the original number
     length = log10(a)+1;
-    // Calculate the sum of cubes of digits
+
     while (a > 0) {
         rem = a % 10; // Extract the last digit
-        sum += pow(rem, length); // Add the cube of the digit to sum
+        sum += pow(rem, length); 
         a /= 10; // Remove the last digit from the number
     }
-    // Check if the sum of cubes of digits equals the original number
+ 
     if (sum == num)
         printf("It is an Armstrong number\n");
     else
